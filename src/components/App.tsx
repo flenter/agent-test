9104a3d6a464c185883f7789050a33b70fc41752
Jacco Flenter <flenter@users.noreply.github.com>
2025-03-12 22:48:22 +0100

    add some db links

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,19 +1,59 @@
 import { useAgent } from "agents-sdk/react"
+import { useCallback, useEffect, useState } from "react";
+import { Button } from "./ui/button";
 
 export function App() {
-  const party = useAgent({
-    onMessage: (message) => {
-      console.log(message);
+  const [_connected, setConnected] = useState(false);
+  const agent = useAgent({
+    onMessage: (message: WebSocketEventMap["message"]) => {
+      console.log('message', message);
+      if (message.data === JSON.stringify("ACK")) {
+        console.log('ACK received');
+      }
+      // message.target?.send(JSON.stringify('ACK'));
     },
     // agent: 'chat',
     agent: "chat",
 
-    onOpen: () => console.log("Connection established"),
+    onOpen: (event) => {
+      console.log("Connection established");
+      setConnected(true);
+    },
     onClose: () => console.log("Connection closed"),
+    onStateUpdate: (state) => console.log("State updated", state),
     onError: (error) => console.error(error),
   });
 
-  console.log('party', party);
+  const { readyState, } = agent;
+
+  console.log('party', readyState);
+  // const ref = useRef(party);
+  // const close = useCallback(() => {
+  //   console.log('closing')
+  //   ref.current.close();
+  // }, [ref]);
+  const close = useCallback(() => agent.close(), [agent.close]);
+  const closeConnection = readyState === WebSocket.OPEN ? () => agent.close() : undefined;
+  useEffect(() => {
+    if (!closeConnection) {
+      return;
+    }
+
+    return () => {
+      closeConnection();
+    }
+  }, [closeConnection]);
+
   return <div>      hello world
+    {readyState === WebSocket.CONNECTING && <div>Connecting...</div>}
+    {readyState === WebSocket.OPEN && <div>Connected!</div>}
+    {readyState === WebSocket.CLOSING && <div>Closing...</div>}
+    {readyState === WebSocket.CLOSED && <div>Closed!</div>}
+    <div className="grid gap-2 grid-cols-2">
+      <Button className="cursor-pointer" onClick={() => {
+        agent.send(JSON.stringify('I want a list user endpoint'));
+      }}>Send</Button>
+      <Button onClick={close}>Close</Button>
+    </div>
   </div>
 }
--- src/server.ts
+++ src/server.ts
@@ -1,68 +1,14 @@
 import { Hono } from "hono";
 import { agentsMiddleware } from "hono-agents";
-import { Agent, type AgentNamespace } from "agents-sdk";
-// import { AIChatAgent } from "agents-sdk/ai-chat-agent";
+export { Chat } from "./Agent"
+// import { ollama } from "ollama-ai-provider";
 
-export type Env = {
-  Chat: AgentNamespace<Chat>;
-};
-
-// Define your agent classes
-export class Chat extends Agent<Env> {
-
-  async onRequest(request) {
-    return new Response("Ready to assist with chat.");
-  }
-  // // async onChatMessage(onFinish: StreamTextOnFinishCallback<{}>) {
-
-  // // }
-  // // Called when an Agent is started (or woken up)
-  // async onStart() {
-  //   // Can access this.env and this.state
-  //   console.log("Agent started");
-  // }
-
-  // // Called when a HTTP request is received
-  // // Can be connected to routeAgentRequest to automatically route
-  // // requests to an individual Agent.
-  // async onRequest(request) {
-  //   console.log("Received request!");
-  // }
-
-  // // Called when a WebSocket connection is established
-  // async onConnect(connection, ctx) {
-  //   console.log("Connected!");
-  //   // Check the request at ctx.request
-  //   // Authenticate the client
-  //   // Give them the OK.
-  //   connection.accept();
-  // }
-
-  // // Called for each message received on the WebSocket connection
-  // async onMessage(connection, message) {
-  //   console.log(`message from client ID: ${connection.id}`);
-  //   // Send messages back to the client
-  //   connection.send("Hello!");
-  // }
-
-  // // WebSocket error and disconnection (close) handling.
-  // async onError(connection, error) {
-  //   console.error(`WS error: ${error}`);
-  // }
-
-  // async onClose(connection, code, reason, wasClean) {
-  //   console.log(`WS closed: ${code} - ${reason} - wasClean: ${wasClean}`);
-  //   connection.close();
-  // }
-
-  // // Called when the Agent's state is updated
-  // // via this.setState or the useAgent hook from the agents-sdk/react package.
-  // async onStateUpdate(state) {
-  //   // 'state' will be typed if you supply a type parameter to the Agent class.
-  // }
-
+interface Env {
+  GOOGLE_GENERATIVE_AI_API_KEY: string;
+  OPENAI_API_KEY: string;
+  Chat: DurableObjectNamespace<typeof Chat>;
+  DB: D1Database;
 }
-
 // export class AssistantAgent extends Agent {
 //   async onRequest(request) {
 //     return new Response("I'm your AI assistant.");
@@ -71,11 +17,6 @@ export class Chat extends Agent<Env> {
 
 // Basic setup
 const app = new Hono();
-app.use("*", (c, next) => {
-  console.log('Chat', Chat)
-  // console.log("Request received", c.req.url, "next", next);
-  return next();
-})
 app.use("*", agentsMiddleware());
 
 // // or with authentication
